d24b08c8241ccee0a6786c8a72c76371ce27675c
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-27 10:54:58 +0530

    commit

---
+++ 12_functions.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+CHECK_ROOT(){
+    if ( USERID -ne 0)
+    then
+    echo "please run the script with root access"
+    exit 1
+ fi   
+}
+
+VALIDATE(){
+    if ( $1 -ne 0 )
+    then
+    echo "$2 is failed"
+    exit 1
+else
+    echo "$2 is success"
+    fi
+}
+
+CHECK_ROOT
+
+dnf list installed git
+
+if ( $? -ne 0 )
+
+    then 
+    echo "git is not installed, going to install"
+
+    dnf install git -y
+    VALIDATE $? "istalling git"
+else
+    echo "git is already installed"
+fi
+
+dnf list installed mysqld
+
+if ( $? -ne 0 )
+
+    then 
+    echo "mysql is not installed"
+
+    dnf install mysqld -y
+    VALIDATE $? "installing mysqld"
+
+else
+    echo  "installing mysql"
+
+fi
+
+
