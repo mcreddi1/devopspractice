006c9f93e6c3007d199dda9738d39d74b860c996
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-29 14:55:58 +0530

    devops practice

--- 18-set.sh
+++ 18-set.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-set -e #setting the automatic error exit
+set -e #setting the automatic exit, when error comes, -ex for debugging mode
+
 echo "hello world"
 echooo "hello world failure"
 echo "hello world success
---
+++ 19-trap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e #auto exit on error and -ex for debugging mode
+
+FAILURE(){
+    echo "failed at: $1:$2"
+
+}
+
+trap 'failre "${LINENO}" "$BASH_COMMAND"' ERR #ERR is the error signal
+
+echo "hello world"
+echoo "hello world failed"
+echo "hello world success
+"
