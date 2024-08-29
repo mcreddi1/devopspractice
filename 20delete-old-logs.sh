5d366d60d3aa43fa76250bc501a6f60dc8f748f0
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-29 16:56:45 +0530

    devops practice

---
+++ 20delete-old-logs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source_dir=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $source_dir ]
+then
+
+     echo  -e "$source_dir $G exists $N"
+else
+     echo -e "$source_dir $R not exists $N"
+     exit 1
+fi
+
+files=$(find $source_dir -name "*.log" -mtime +14)
+echo "files: $files"
+
+while IFS=read -r line #IFS: internal field separator empty it will ignore the space, -r is to not ignore special characters like /
+
+do
+
+  echo "deleting line: $line"
+  rm -rf $line
+done <<< $files
