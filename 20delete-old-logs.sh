8d411d121609b0127a28bc747a62fb2a349108c3
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-29 18:20:40 +0530

    devops practice

--- 20delete-old-logs.sh
+++ 20delete-old-logs.sh
@@ -19,10 +19,10 @@ fi
 files=$(find $source_dir -name "*.log" -mtime +14)
 echo "files: $files"
 
-while IFS= read -r line #IFS: internal field separator empty it will ignore the space, -r is to not ignore special characters like /
+while IFS= read -r file #IFS: internal field separator empty it will ignore the space, -r is to not ignore special characters like /
 
 do
 
-  echo "deleting line: $line"
-  rm -rf $line
+  echo "deleting line: $file"
+  rm -rf $file
 done <<< $files
