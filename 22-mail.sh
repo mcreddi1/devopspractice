482d553952098cd99983b8703384bef95b0ca4b1
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-30 19:05:54 +0530

    devops practice

--- 22-mail.sh
+++ 22-mail.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 disk_usage=$(df -hT | grep xfs)
-disc_threshhold=>5
+disc_threshhold=5
 
 
 while IFS= read -r line 
 
 do
     usage=$(echo $line | grep xfs | awk -F " " '{print -6F}' | cut -d "%" -f1)
+
     partition=$(echo $line | grep xfs | awk '{print $NF}')
-    if usage=$disk_usage -gt $disc_threshhold
+
+    if usage=$disk_usage -ge $disc_threshhold
 then
     echo "$partition exceeds $disc_threshhold, current value: $usage"
     fi
