f3442575cc92e590898cdbac3ad041d4969a4872
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-30 19:52:23 +0530

    devops practice

--- 20delete-old-logs.sh
+++ 20delete-old-logs.sh
@@ -22,5 +22,5 @@ echo "files: $log_files"
 while IFS= read -r files #IFS: internal field separator empty it will ignore the space, -r is to not ignore special characters like /
 do
   echo "deleting files: $log_files"
-  rm -rf $files
+  rm -rf $done <<< $log_files
 done <<< $log_files
