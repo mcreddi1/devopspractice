fc7d8905b3fbb32baba626ad6f88f14d9feabb3c
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-30 19:13:20 +0530

    devops practice

--- 22-mail.sh
+++ 22-mail.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-disk_usage=$(df -hT | grep xfs)
-disc_threshhold=5
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
 
 
-while IFS= read -r line 
+#while IFS= read -r line 
+#
+#do
+#    USAGE=$(echo $line | grep xfs | awk -F " " '{print -6F}' | cut -d "%" -f1)
+#
+#    PARTITION=$(echo $line | grep xfs | awk '{print $NF}')
+#
+#    if [ $USAGE -ge $DISK_THRESHOLD ]
+#    then
+#    echo "$PARTITION exceeds $DISK_THRESHOLD, current value: $USAGE"
+#    fi
+#done <<< $DISK_USAGE
 
+while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
-    usage=$(echo $line | grep xfs | awk -F " " '{print -6F}' | cut -d "%" -f1)
-
-    partition=$(echo $line | grep xfs | awk '{print $NF}')
-
-   # if [ $usage -ge $disc_threshhold ]
-   # then
-   # echo "$partition exceeds $disc_threshhold, current value: $usage"
-   # fi
-done <<< $disk_usage
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
+    fi
+done <<< $DISK_USAGE
