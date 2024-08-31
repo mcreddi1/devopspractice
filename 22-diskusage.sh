08eee03d5d10abbb7f6097a0a377f1948ceeee0a
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-31 11:11:37 +0530

    devops practice

--- 22-diskusage.sh
+++ 22-diskusage.sh
@@ -13,3 +13,17 @@ do
         echo "$PARTITION exceeds $DISK_THRESHOLD, current value: $USAGE"
     fi
 done <<< $DISK_USAGE
+
+# Set recipient and email details
+recipient="chinnapareddymora@ge.com"
+subject="Disk Usage Alert"
+body="Disk usage is high. Details:"
+
+# Get disk usage information
+disk_usage=$(df -h)
+
+# Append disk usage info to the body
+body="$body\n\n$disk_usage"
+
+# Send the email
+echo -e "$body" | mail -s "$subject" "$recipient"
